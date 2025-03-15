b42f0e10e745f729e5faca0f7b45de8192f185d8
asadpro888 <asilbekismoilov220@gmail.com>
2025-03-15 18:10:12 +0300

    first commit

---
+++ clerk-react/src/App.tsx
@@ -0,0 +1,19 @@
+import SignIn from "./SingIn"
+const App = () => {
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, eius.</li>
+          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, eius.</li>
+          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, eius.</li>
+          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, eius.</li>
+          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, eius.</li>
+        </ul>
+      </div>
+      <SignIn/>
+    </div>
+  )
+}
+
+export default App
